ca21bd3c72368654fa0625f27aa88491f45987d2
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-03 13:04:29 +0300

    Well, need to figure it out

--- routes/auth/sign_up.js
+++ routes/auth/sign_up.js
@@ -13,6 +13,7 @@ const logger = pino({
 
 const UserModel = require('../../models/user.js');
 const passport = require("passport");
+const LocalStrategy  = require('passport-local').Strategy;
 
 const expressLogger = expressPino({logger});
 
@@ -29,11 +30,55 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-
-
 router.post('/', function(req, res, next) {
 
-    var isRegistered = 0;
+    passport.use('signup', new LocalStrategy({
+            passReqToCallback : true
+        },
+        function(req, username, password, done) {
+            findOrCreateUser = function(){
+                // поиск пользователя в Mongo с помощью предоставленного имени пользователя
+                UserModel.findOne({'login':username},function(err, user) {
+                    // В случае любых ошибок - возврат
+                    if (err){
+                        console.log('Error in SignUp: '+err);
+                        return done(err);
+                    }
+                    // уже существует
+                    if (user) {
+                        console.log('User already exists');
+                        return done(null, false,
+                            req.flash('message','User Already Exists'));
+                    } else {
+                        // если пользователя с таки адресом электронной почты
+                        // в базе не существует, создать пользователя
+                        var newUser = new User();
+                        // установка локальных прав доступа пользователя
+                        newUser.username = username;
+                        newUser.password = createHash(password);
+                        newUser.email = req.param('email');
+                        newUser.firstName = req.param('firstName');
+                        newUser.lastName = req.param('lastName');
+
+                        // сохранения пользователя
+                        newUser.save(function(err) {
+                            if (err){
+                                console.log('Error in Saving user: '+err);
+                                throw err;
+                            }
+                            console.log('User Registration succesful');
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            };
+
+            // Отложить исполнение findOrCreateUser и выполнить
+            // метод на следующем этапе цикла события
+            process.nextTick(findOrCreateUser);
+        });
+);
+    /*
     var checkUser;
     if(req.query.login != null && req.query.password != null) {
 
@@ -58,7 +103,6 @@ router.post('/', function(req, res, next) {
     }
 
 
-/*
     newUser.save(function (err) {
         if(err)
             return console.log(err);
