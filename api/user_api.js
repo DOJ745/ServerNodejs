b995431303df995f09fa72c4a1996ea159072e03
DOJ745 <andreyfa12@mail.ru>
2021-05-10 19:39:08 +0300

    Added update methods for user_api.js

--- api/user_api.js
+++ api/user_api.js
@@ -17,7 +17,10 @@ exports.createUser = function(reqLogin, reqPassword) {
 }
 
 exports.getUserByLogin = function(reqLogin, res) {
-    User.findOne({login: reqLogin}, function(err, user) {
+
+    User.findOne(
+        {login: reqLogin},
+        function(err, user) {
         if (err)
             console.log("Error with getting user!\n" + err);
         console.log("Found user BY LOGIN:\n" + user);
@@ -28,7 +31,10 @@ exports.getUserByLogin = function(reqLogin, res) {
 let errorUser = {login: "error", password: "error"};
 
 exports.checkUser = function(reqLogin, reqPassword, req, res) {
-    User.findOne({login: reqLogin}, function (err, user) {
+
+    User.findOne(
+        {login: reqLogin},
+        function (err, user) {
 
         if(err) { console.log("Error with getting user!\n" + err); }
         if(user.password === hash(reqPassword)) {
@@ -41,15 +47,50 @@ exports.checkUser = function(reqLogin, reqPassword, req, res) {
 }
 
 exports.logoutUser = function (id) {
-    Session.findByIdAndDelete({_id: id}, function(err, session) {
+
+    Session.findByIdAndDelete(
+        {_id: id},
+        function(err, session) {
 
         if(err) { console.log("\n*** Error with finding session!\n" + err); }
         console.log("\n*** Session has been deleted ***: " + session);
     })
 }
 
-//exports.deleteUser
-//exports.updateUser
+exports.deleteUser = function (reqLogin, res) {
+
+    User.findOneAndDelete(
+        {login: reqLogin},
+        function (err, delObj) {
+        if(err) {
+            res.send({isDeleted: 1, error: err, object: "none"});
+            return console.log("\n*** Error with deleting user!\n" + err);
+        }
+        else {
+            console.log("\n*** User successfully deleted!\n");
+            res.send({isDeleted: 0, error: "none", object: delObj});
+        }
+    });
+}
+
+exports.updateUser = function (reqLogin, reqScore, res) {
+
+    User.findOneAndUpdate(
+        {login:reqLogin},
+        {score: reqScore},
+        {new: true},
+        function (err, updObj) {
+
+            if(err) {
+                res.send({isUpdated: 1, error: err, object: "none"});
+                return console.log("\n*** Error with updating user!\n" + err);
+            }
+            else {
+                console.log("\n*** User successfully updated!\n");
+                res.send({isUpdated: 0, error: "none", object: updObj});
+            }
+    });
+}
 
 function hash(text) {
     return crypto.createHash('sha1').update(text).digest('base64');
