17cb586180132849476910bbe71dcea00467c117
DOJ745 <andreyfa12@mail.ru>
2021-05-10 18:57:01 +0300

    Going to do simple update. Because I don't need cascade, cause I don't change the ID of theme, or question or difficulty

---
+++ routes/db/crud/update.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({
+    level: process.env.LOG_LEVEL || 'debug',
+    prettyPrint: {
+        colorize: true
+    }
+} );
+
+const expressLogger = expressPino({logger});
+const app = express();
+app.use(expressLogger);
+
+
+const Theme = require('../../../models/theme');
+const Answers = require('../../../models/answers');
+const Question = require('../../../models/question');
+const Difficulty = require('../../../models/difficulty');
+const League = require('../../../models/league');
